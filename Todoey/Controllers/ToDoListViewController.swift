aa96cd0b3d9a4066ff50c711059f40bfff01b69d
Yasir Khan <mr_yasir_khan52@yahoo.com>
2019-10-27 13:25:18 +0500

    Converted to MVC Design Pattern

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -13,13 +13,27 @@ class ToDoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       if let  items =   defaults.array(forKey: "TodoListArray") as? [String]
-       {
-        itemArray = items
+        
+        
+        let newItem = Item()
+        newItem.title = "Karachi"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Lahore"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Islamabad"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item]
+        {
+            itemArray  = items
         }
-        // Do any additional setup after loading the view.
+ 
     }
-    var itemArray = ["Karachi", "Lahore", "Islamabad"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
     
     
@@ -32,27 +46,32 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
-        return cell
         
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        //MARK -  Using Ternary Operator instead of IF ELSE Statements
+        
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        
+//        if item.done == true{
+//            cell.accessoryType = .checkmark
+//        }else
+//        {
+//            cell.accessoryType = .none
+//
+//        }
+        return cell
     }
-    
-   
+
     //MARK - Table View Delegate Method
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
-        
-        
-        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark)
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
     }
     
     //MARK - ADD Button Action
@@ -61,19 +80,19 @@ class ToDoListViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            print(self.itemArray)
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
-        
-            
             }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create Todoey"
             textField = alertTextField
             
         }
-        
-            
             alert.addAction(action)
             self.present(alert, animated: true, completion: nil)
             
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by mac on 27/10/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Item
+{
+    var title : String = ""
+    
+    var done : Bool = false
+    
+    
+}
